49339036b1d953cafbf5c667082271f1b87f00e2
RonaldJobse <ronald.jobse@vng.nl>
2025-12-12 15:55:29 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,26 +2,34 @@ let respecConfig = {
 //  useLogo: true,
 //  useLabel: true,
 //  license: "eupl",
-  shortName: "Respec-template",
-  pubDomain: "hl",
+  shortName: "Respec-im-MijnTaken",
+  pubDomain: "im",
 
   // Zie de globale property 'localizationStrings/nl' voor de lijst met toegestane specificatie-types
   specType: "HL",
   
   // Zie de globale property 'localizationStrings/nl' voor de lijst met toegestane specificatie-statussen
   specStatus: "IO",
-  publishDate: "2024-05-16",
-  publishVersion: "0.2.0",
+  publishDate: "2025-12-10",
+  publishVersion: "0.1.0",
 
   // Zie de globale property 'localizationStrings/nl' voor de lijst met toegestane maturities
   //previousMaturity: "IO",
   //previousPublishDate: "2022-06-24",
   //previousPublishVersion: "0.0.4",
   
-  title: "Respec documentatie generatie",
-  subtitle: "Tevens een voorbeeld van een gegenereerd Respec document",
-  content: {"mermaid": "", "ch01": "informative", "ch02": ""},
-//  authors:
+  title: "Informatiemodel MijnTaken",
+  // subtitle: "Tevens een voorbeeld van een gegenereerd Respec document",
+  // content: {"mermaid": "", "ch01": "informative", "ch02": ""},
+  authors:
+    [
+      {
+        name: "Ronald Jobse",
+        company: "VNG Realisatie",
+        companyURL: "https://vng.nl/artikelen/vng-realisatie",
+      }
+    ],
+//  editors:
 //    [
 //      {
 //        name: "Robert Melskens",
@@ -29,39 +37,31 @@ let respecConfig = {
 //        companyURL: "https://vng.nl/artikelen/vng-realisatie",
 //      }
 //    ],
-  editors:
-    [
-      {
-        name: "Robert Melskens",
-        company: "VNG Realisatie",
-        companyURL: "https://vng.nl/artikelen/vng-realisatie",
-      }
-    ],
-  github: "https://github.com/VNG-Realisatie/VNG-R-Respec-Template",
+  github: "https://github.com/VNG-Realisatie/MijnTaken-Respec",
 
-  //  maxTocLevel: 2,
+    maxTocLevel: 2,
 
   // Creëer PDF en link deze aan de file in de header van het html document (optioneel). Het is (nog) niet mogelijk hier een globale property van te maken:
   alternateFormats: [
       {
           label: "pdf",
-          uri: "hl-Respec-template-0.2.0.pdf",
+          uri: "im-MijnTaken-0.1.0.pdf",
   //        uri: "Respec-bij-VNG-R.pdf",
       },
     ],
-  localBiblio: {
-        "MIM": {
-           "href": "https://docs.geostandaarden.nl/mim/mim/",
-           "publisher": "Geonovum",
-           "title": "MIM - Metamodel Informatie Modellering",
-           "date": "Oktober 2023",
-           "rawDate": "2023"
-        },
-        "SemVer": {
-           "href": "https://semver.org/lang/nl/",
-           "title": "Semantisch Versioneren 2.0.0",
-           "date": "December 19, 2023",
-           "rawDate": "2023"
-        },
-    },
+//  localBiblio: {
+//        "MIM": {
+//           "href": "https://docs.geostandaarden.nl/mim/mim/",
+//           "publisher": "Geonovum",
+//           "title": "MIM - Metamodel Informatie Modellering",
+//           "date": "Oktober 2023",
+//           "rawDate": "2023"
+//        },
+//        "SemVer": {
+//           "href": "https://semver.org/lang/nl/",
+//           "title": "Semantisch Versioneren 2.0.0",
+//           "date": "December 19, 2023",
+//           "rawDate": "2023"
+//        },
+//    },
 }
